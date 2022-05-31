34e685a0f50cadddbb90e6e57fc60b76b8263e43
Stephany Carolina <ster.carolinas@hotmail.com>
2022-05-30 21:21:33 -0300

    alteração na home

--- src/components/Home/HotelHome.vue
+++ src/components/Home/HotelHome.vue
@@ -69,7 +69,7 @@
     </v-parallax>
     <div></div>
 
-    <div style="margin:3%">
+      <div style="margin:3%">
       <v-row align="center" justify="center">
         <v-col class="text-center" cols="12">
       
@@ -126,8 +126,8 @@
             OK
           </v-btn>
         </v-date-picker>
-        
       </v-menu>
+
     
         
       </v-col>
@@ -207,6 +207,57 @@
 
 </div>
     <!--fim data de check in e checkout busca-->
+
+    <div>
+    <v-parallax
+      src="https://static.wixstatic.com/media/8a3e6d_6a199168c8a045b3a68fd392dd57594b.jpg/v1/fill/w_1028,h_692,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/8a3e6d_6a199168c8a045b3a68fd392dd57594b.jpg"
+    >
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <div>
+            <h1
+              style="
+                color: black;
+                align: center;
+                margin-left: 40%;
+                margin-right: 40%;
+                background-color: white;
+              "
+              tclass="text-h4 font-weight-thin mb-4"
+            >
+              Seja Bem-Vindo!
+            </h1>
+            <h3
+              style="
+                color: black;
+                align: center;
+                margin-left: 40%;
+                margin-right: 40%;
+                background-color: white;
+              "
+              tclass="text-h4 font-weight-thin mb-4"
+            >
+              <v-card-text>
+                The navigation drawer will appear from the bottom on smaller
+                size screens.
+              </v-card-text>
+              <v-card-text>
+                The navigation drawer will appear from the bottom on smaller
+                size screens.
+              </v-card-text>
+              <v-card-text>
+                The navigation drawer will appear from the bottom on smaller
+                size screens.
+              </v-card-text>
+            </h3>
+          </div>
+          <div>
+            <v-card-text> </v-card-text>
+          </div>
+        </v-col>
+      </v-row>
+    </v-parallax>
+  </div>
 
     <v-footer padless>
       <v-col class="text-center" cols="12">
