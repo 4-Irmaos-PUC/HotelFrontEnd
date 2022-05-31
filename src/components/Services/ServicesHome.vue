ca1529bf59c4300986895665707aca3eb3cd5d59
Geovanna Rafaela da Silva Souza <geovanna.rafaela@totvs.com.br>
2022-05-31 10:33:58 -0300

    Alterações tela de serviço

--- src/components/Services/ServicesHome.vue
+++ src/components/Services/ServicesHome.vue
@@ -1,54 +1,81 @@
 <template>
-  <div>
-    <v-parallax
-      src="https://static.wixstatic.com/media/8a3e6d_6a199168c8a045b3a68fd392dd57594b.jpg/v1/fill/w_1028,h_692,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/8a3e6d_6a199168c8a045b3a68fd392dd57594b.jpg"
-    >
-      <v-row align="center" justify="center">
-        <v-col class="text-center" cols="12">
-          <div>
-            <h1
-              style="
-                color: black;
-                align: center;
-                margin-left: 40%;
-                margin-right: 40%;
-                background-color: white;
-              "
-              tclass="text-h4 font-weight-thin mb-4"
-            >
-              Seja Bem-Vindo!
-            </h1>
-            <h3
-              style="
-                color: black;
-                align: center;
-                margin-left: 40%;
-                margin-right: 40%;
-                background-color: white;
-              "
-              tclass="text-h4 font-weight-thin mb-4"
-            >
-              <v-card-text>
-                The navigation drawer will appear from the bottom on smaller
-                size screens.
-              </v-card-text>
-              <v-card-text>
-                The navigation drawer will appear from the bottom on smaller
-                size screens.
-              </v-card-text>
-              <v-card-text>
-                The navigation drawer will appear from the bottom on smaller
-                size screens.
-              </v-card-text>
-            </h3>
-          </div>
-          <div>
-            <v-card-text> </v-card-text>
-          </div>
-        </v-col>
-      </v-row>
-    </v-parallax>
-  </div>
+<div class="div">
+  <v-row>
+  
+  <v-card class="mx-auto" max-width="344">
+    <v-card-text>
+      <div>Word of the Day</div>
+      <p class="text-h4 text--primary">be•nev•o•lent</p>
+      <p>adjective</p>
+      <div class="text--primary">well meaning and kindly.<br>"a benevolent smile"</div>
+    </v-card-text>
+  </v-card>
+  
+  
+
+
+  <v-card class="mx-auto" max-width="344">
+    <v-card-text>
+      <div>Word of the Day</div>
+      <p class="text-h4 text--primary">be•nev•o•lent</p>
+      <p>adjective</p>
+      <div class="text--primary">well meaning and kindly.<br>"a benevolent smile"</div>
+    </v-card-text>
+  </v-card>
+
+
+
+
+  <v-card class="mx-auto" max-width="344">
+    <v-card-text>
+      <div>Word of the Day</div>
+      <p class="text-h4 text--primary">be•nev•o•lent</p>
+      <p>adjective</p>
+      <div class="text--primary">well meaning and kindly.<br>"a benevolent smile"</div>
+    </v-card-text>
+  </v-card>
+  
+  </v-row>
+</div>
+
+<div class="div">
+  <v-row>
+  
+  <v-card class="mx-auto" max-width="344">
+    <v-card-text>
+      <div>Word of the Day</div>
+      <p class="text-h4 text--primary">be•nev•o•lent</p>
+      <p>adjective</p>
+      <div class="text--primary">well meaning and kindly.<br>"a benevolent smile"</div>
+    </v-card-text>
+  </v-card>
+  
+  
+
+
+  <v-card class="mx-auto" max-width="344">
+    <v-card-text>
+      <div>Word of the Day</div>
+      <p class="text-h4 text--primary">be•nev•o•lent</p>
+      <p>adjective</p>
+      <div class="text--primary">well meaning and kindly.<br>"a benevolent smile"</div>
+    </v-card-text>
+  </v-card>
+
+
+
+
+  <v-card class="mx-auto" max-width="344">
+    <v-card-text>
+      <div>Word of the Day</div>
+      <p class="text-h4 text--primary">be•nev•o•lent</p>
+      <p>adjective</p>
+      <div class="text--primary">well meaning and kindly.<br>"a benevolent smile"</div>
+    </v-card-text>
+  </v-card>
+  
+  </v-row>
+</div>
 </template>
 
 <script>
